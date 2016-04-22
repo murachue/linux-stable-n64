e594bc38abc3ae76f5104b174dcb498853d2eade
Murachue <murachue+github@gmail.com>
2016-04-23 03:08:19 +0900

    n64: compilable IRQ handling, but no testing.

--- arch/mips/nintendo64/n64.c
+++ arch/mips/nintendo64/n64.c
@@ -14,6 +14,7 @@ arch/mips/built-in.o: In function `free_initmem':
 */
 
 #include <linux/io.h> //set_io_port_base
+#include <linux/of_address.h> //of_address_to_resource
 #include <linux/of_fdt.h> //of_scan_flat_dt, __dtb_start
 #include <linux/of_irq.h> //of_irq_init
 #include <linux/sizes.h> //SZ_1M
@@ -23,38 +24,134 @@ arch/mips/built-in.o: In function `free_initmem':
 #include <asm/prom.h> //__dt_setup_arch
 #include <asm/time.h> //mips_hpt_frequency
 
+// called from arch/mips/kernel/proc.c
 const char *get_system_type(void) {
 	// TODO: refer dts:model?
 	return "Nintendo 64";
 }
 
+static void __iomem *rt_intc_membase;
+
+static void ralink_intc_irq_unmask(struct irq_data *d)
+{
+	__raw_writel(BIT(d->hwirq * 2 + 1), rt_intc_membase + 0x0C); // MI_INTR_MASK_REG
+}
+
+static void ralink_intc_irq_mask(struct irq_data *d)
+{
+	__raw_writel(BIT(d->hwirq * 2 + 0), rt_intc_membase + 0x0C); // MI_INTR_MASK_REG
+}
+
+static struct irq_chip ralink_intc_irq_chip = {
+	.name		= "N64_MI",
+	.irq_unmask	= ralink_intc_irq_unmask,
+	.irq_mask	= ralink_intc_irq_mask,
+	.irq_mask_ack	= ralink_intc_irq_mask, /* TODO mmm, each device has clear intr ... */
+};
+
+static int intc_map(struct irq_domain *d, unsigned int irq, irq_hw_number_t hw)
+{
+	irq_set_chip_and_handler(irq, &ralink_intc_irq_chip, handle_level_irq);
+
+	return 0;
+}
+
+static const struct irq_domain_ops irq_domain_ops = {
+	.xlate = irq_domain_xlate_onecell,
+	.map = intc_map,
+};
+
+static void ralink_intc_irq_handler(struct irq_desc *desc)
+{
+	u32 pending = __raw_readl(rt_intc_membase + 0x08); // MI_INTR_REG
+
+	if (pending) {
+		struct irq_domain *domain = irq_desc_get_handler_data(desc);
+		generic_handle_irq(irq_find_mapping(domain, __ffs(pending)));
+	} else {
+		spurious_interrupt();
+	}
+}
+
+static int __init nintendo_mi_of_init(struct device_node *of_node, struct device_node *parent) {
+#if 0
+	struct irq_domain *domain;
+
+	/* Mask interrupts. */
+	*0 = 0xffff00ff;
+
+	domain = irq_domain_add_legacy(of_node, 8, MIPS_CPU_IRQ_BASE, 0, &mips_cpu_intc_irq_domain_ops, NULL);
+	if (!domain)
+		panic("Failed to add irqdomain for Nintendo 64 MI");
+#else
+	struct resource res;
+	struct irq_domain *domain;
+	int irq;
+
+	irq = irq_of_parse_and_map(of_node, 0);
+	if (!irq)
+		panic("Failed to get INTC IRQ");
+
+	if (of_address_to_resource(of_node, 0, &res))
+		panic("Failed to get intc memory range");
+
+	if (request_mem_region(res.start, resource_size(&res),
+				res.name) < 0)
+		pr_err("Failed to request intc memory");
+
+	rt_intc_membase = ioremap_nocache(res.start,
+					resource_size(&res));
+	if (!rt_intc_membase)
+		panic("Failed to remap intc memory");
+
+	/* disable all interrupts */
+	__raw_writel(0x555, rt_intc_membase + 0x0C); // MI_INTR_MASK_REG
+
+	domain = irq_domain_add_legacy(of_node, 6/*count*/, 8/*base*/, 0, &irq_domain_ops, NULL);
+	if (!domain)
+		panic("Failed to add irqdomain MI intrs");
+
+	irq_set_chained_handler_and_data(irq, ralink_intc_irq_handler, domain);
+
+	/* tell the kernel which irq is used for performance monitoring */
+	//rt_perfcount_irq = irq_create_mapping(domain, 9);
+
+	return 0;
+#endif
+}
+
 static struct of_device_id __initdata of_irq_ids[] = {
 	{ .compatible = "mti,cpu-interrupt-controller", .data = mips_cpu_irq_of_init },
-//	{ .compatible = "ralink,rt2880-intc", .data = intc_of_init },
+	{ .compatible = "nintendo,mi", .data = nintendo_mi_of_init },
 	{},
 };
 
 void __init arch_init_irq(void)
 {
+	// we can call irqchip_init() here, but N64 does not need to be such general...
 	of_irq_init(of_irq_ids);
 }
 
 void __init prom_init(void)
 {
-	//int argc;
-	//char **argv;
+/*
+	int argc;
+	char **argv;
 
-	//prom_soc_init(&soc_info);
+	prom_soc_init(&soc_info);
 
-	//pr_info("SoC Type: %s\n", get_system_type());
+	pr_info("SoC Type: %s\n", get_system_type());
 
-	//prom_init_cmdline(argc, argv);
+	prom_init_cmdline(argc, argv);
+*/
 }
 
+// called from arch/mips/kernel/setup.c
 void __init device_tree_init(void)
 {
 	unflatten_and_copy_device_tree();
 }
+
 static int memory_dtb;
 
 static int __init early_init_dt_find_memory(unsigned long node,
