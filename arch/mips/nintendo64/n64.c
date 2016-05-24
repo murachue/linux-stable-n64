dd8d98c05c4418131f3244457d12f638485372e9
Murachue <murachue+github@gmail.com>
2016-05-24 09:02:54 +0900

    n64: rename, support no USE_OF, memsize from bootloader.
    * rename funcs, from referenced ralink-ish.
    * support configuration without USE_OF.
    * get memory size by fw_arg0 that is passed by the boot loader.

--- arch/mips/nintendo64/n64.c
+++ arch/mips/nintendo64/n64.c
@@ -1,22 +1,18 @@
 /*
-arch/mips/built-in.o: In function `show_cpuinfo':
-proc.c:(.text+0xc6d0): undefined reference to `get_system_type'
-arch/mips/built-in.o: In function `init_IRQ':
-(.init.text+0x2e4): undefined reference to `arch_init_irq'
-arch/mips/built-in.o: In function `setup_arch':
-(.init.text+0x898): undefined reference to `prom_init'
-arch/mips/built-in.o: In function `setup_arch':
-(.init.text+0x8bc): undefined reference to `plat_mem_setup'
-arch/mips/built-in.o: In function `time_init':
-(.init.text+0xf2c): undefined reference to `plat_time_init'
-arch/mips/built-in.o: In function `free_initmem':
-(.ref.text+0x28): undefined reference to `prom_free_prom_memory'
-*/
+ * Nintendo 64 arch-dependent code
+ * Copyright (c) 2016 Murachue <murachue+github@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ */
 
 #include <linux/io.h> //set_io_port_base
+#ifdef CONFIG_USE_OF
 #include <linux/of_address.h> //of_address_to_resource
 #include <linux/of_fdt.h> //of_scan_flat_dt, __dtb_start
 #include <linux/of_irq.h> //of_irq_init
+#endif
 #include <linux/sizes.h> //SZ_1M
 #include <asm/addrspace.h> //KSEG1
 #include <asm/bootinfo.h> //detect_memory_region
@@ -24,46 +20,50 @@ arch/mips/built-in.o: In function `free_initmem':
 #include <asm/prom.h> //__dt_setup_arch
 #include <asm/time.h> //mips_hpt_frequency
 
-// called from arch/mips/kernel/proc.c
+// called from arch/mips/kernel/proc.c:show_cpuinfo, for /proc/cpuinfo
 const char *get_system_type(void) {
 	// TODO: refer dts:model?
 	return "Nintendo 64";
 }
 
-static void __iomem *rt_intc_membase;
+static void __iomem *mi_membase;
 
-static void ralink_intc_irq_unmask(struct irq_data *d)
+static void mi_irq_unmask(struct irq_data *d)
 {
-	__raw_writel(BIT(d->hwirq * 2 + 1), rt_intc_membase + 0x0C); // MI_INTR_MASK_REG
+	__raw_writel(BIT(d->hwirq * 2 + 1), mi_membase + 0x0C); // MI_INTR_MASK_REG
 }
 
-static void ralink_intc_irq_mask(struct irq_data *d)
+static void mi_irq_mask(struct irq_data *d)
 {
-	__raw_writel(BIT(d->hwirq * 2 + 0), rt_intc_membase + 0x0C); // MI_INTR_MASK_REG
+	__raw_writel(BIT(d->hwirq * 2 + 0), mi_membase + 0x0C); // MI_INTR_MASK_REG
 }
 
-static struct irq_chip ralink_intc_irq_chip = {
-	.name		= "N64_MI",
-	.irq_unmask	= ralink_intc_irq_unmask,
-	.irq_mask	= ralink_intc_irq_mask,
-	.irq_mask_ack	= ralink_intc_irq_mask, /* TODO mmm, each device has clear intr ... */
+static struct irq_chip mi_irq_chip = {
+	.name		= "n64_mi",
+	.irq_unmask	= mi_irq_unmask,
+	.irq_mask	= mi_irq_mask,
+	.irq_mask_ack	= mi_irq_mask, /* TODO mmm, each device has clear intr ... */
 };
 
-static int intc_map(struct irq_domain *d, unsigned int irq, irq_hw_number_t hw)
+static int mi_intc_map(struct irq_domain *d, unsigned int irq, irq_hw_number_t hw)
 {
-	irq_set_chip_and_handler(irq, &ralink_intc_irq_chip, handle_level_irq);
+	irq_set_chip_and_handler(irq, &mi_irq_chip, handle_level_irq);
 
 	return 0;
 }
 
 static const struct irq_domain_ops irq_domain_ops = {
+#ifdef CONFIG_USE_OF
 	.xlate = irq_domain_xlate_onecell,
-	.map = intc_map,
+	.map = mi_intc_map,
+#else
+	.map = mi_intc_map,
+#endif
 };
 
-static void ralink_intc_irq_handler(struct irq_desc *desc)
+static void mi_irq_handler(struct irq_desc *desc)
 {
-	u32 pending = __raw_readl(rt_intc_membase + 0x08); // MI_INTR_REG
+	u32 pending = __raw_readl(mi_membase + 0x08); // MI_INTR_REG
 
 	if (pending) {
 		struct irq_domain *domain = irq_desc_get_handler_data(desc);
@@ -73,19 +73,46 @@ static void ralink_intc_irq_handler(struct irq_desc *desc)
 	}
 }
 
+static int __init nintendo_mi_init(struct device_node *of_node, struct resource *pres, int irq) {
+	struct irq_domain *domain;
+
+	if (request_mem_region(pres->start, resource_size(pres), pres->name) < 0) {
+		pr_err("n64: Failed to request intc memory");
+		goto out_reqmemreg;
+	}
+
+	mi_membase = ioremap_nocache(pres->start, resource_size(pres));
+	if (!mi_membase) {
+		pr_err("n64: Failed to remap intc memory");
+		goto out_ioremap;
+	}
+
+	/* disable all interrupts */
+	__raw_writel(0x555, mi_membase + 0x0C); // MI_INTR_MASK_REG
+
+	domain = irq_domain_add_legacy(of_node, 6/*count*/, 8/*base*/, 0, &irq_domain_ops, NULL);
+	if (!domain) {
+		pr_err("n64: Failed to add irqdomain MI intrs");
+		goto out_irqdom;
+	}
+
+	irq_set_chained_handler_and_data(irq, mi_irq_handler, domain);
+
+	/* tell the kernel which irq is used for performance monitoring */
+	//rt_perfcount_irq = irq_create_mapping(domain, 9);
+
+	return 0;
+out_irqdom:
+	iounmap(mi_membase);
+out_ioremap:
+	release_mem_region(pres->start, resource_size(pres));
+out_reqmemreg:
+	return -ENOMEM;
+}
+
+#ifdef CONFIG_USE_OF
 static int __init nintendo_mi_of_init(struct device_node *of_node, struct device_node *parent) {
-#if 0
-	struct irq_domain *domain;
-
-	/* Mask interrupts. */
-	*0 = 0xffff00ff;
-
-	domain = irq_domain_add_legacy(of_node, 8, MIPS_CPU_IRQ_BASE, 0, &mips_cpu_intc_irq_domain_ops, NULL);
-	if (!domain)
-		panic("Failed to add irqdomain for Nintendo 64 MI");
-#else
 	struct resource res;
-	struct irq_domain *domain;
 	int irq;
 
 	irq = irq_of_parse_and_map(of_node, 0);
@@ -95,29 +122,7 @@ static int __init nintendo_mi_of_init(struct device_node *of_node, struct device
 	if (of_address_to_resource(of_node, 0, &res))
 		panic("Failed to get intc memory range");
 
-	if (request_mem_region(res.start, resource_size(&res),
-				res.name) < 0)
-		pr_err("Failed to request intc memory");
-
-	rt_intc_membase = ioremap_nocache(res.start,
-					resource_size(&res));
-	if (!rt_intc_membase)
-		panic("Failed to remap intc memory");
-
-	/* disable all interrupts */
-	__raw_writel(0x555, rt_intc_membase + 0x0C); // MI_INTR_MASK_REG
-
-	domain = irq_domain_add_legacy(of_node, 6/*count*/, 8/*base*/, 0, &irq_domain_ops, NULL);
-	if (!domain)
-		panic("Failed to add irqdomain MI intrs");
-
-	irq_set_chained_handler_and_data(irq, ralink_intc_irq_handler, domain);
-
-	/* tell the kernel which irq is used for performance monitoring */
-	//rt_perfcount_irq = irq_create_mapping(domain, 9);
-
-	return 0;
-#endif
+	return nintendo_mi_init(of_node, &res, irq);
 }
 
 static struct of_device_id __initdata of_irq_ids[] = {
@@ -125,13 +130,39 @@ static struct of_device_id __initdata of_irq_ids[] = {
 	{ .compatible = "nintendo,mi", .data = nintendo_mi_of_init },
 	{},
 };
+#endif
 
+#ifndef CONFIG_USE_OF
+/*
+static struct irqaction cpu_ip2_cascade_action = {
+	.handler	= no_action,
+	.name		= "cascade_ip2",
+	.flags		= IRQF_NO_THREAD,
+};
+*/
+#endif
+
+// called from arch/mips/kernel/irq.c:init_IRQ
 void __init arch_init_irq(void)
 {
+#ifdef CONFIG_USE_OF
 	// we can call irqchip_init() here, but N64 does not need to be such general...
 	of_irq_init(of_irq_ids);
+#else
+	mips_cpu_irq_init();
+	{
+		struct resource res = {
+			.name = "irq",
+			.flags = IORESOURCE_MEM,
+			.start = 0x04300000,
+			.end   = 0x0430000F,
+		};
+		nintendo_mi_init(NULL, &res, 2);
+	}
+#endif
 }
 
+// called from arch/mips/kernel/setup.c:setup_arch
 void __init prom_init(void)
 {
 /*
@@ -146,7 +177,8 @@ void __init prom_init(void)
 */
 }
 
-// called from arch/mips/kernel/setup.c
+#ifdef CONFIG_USE_OF
+// called from arch/mips/kernel/setup.c:arch_mem_init
 void __init device_tree_init(void)
 {
 	unflatten_and_copy_device_tree();
@@ -162,11 +194,14 @@ static int __init early_init_dt_find_memory(unsigned long node,
 
 	return 0;
 }
+#endif
 
+// called from arch/mips/kernel/setup.c:setup_arch
 void __init plat_mem_setup(void)
 {
 	set_io_port_base(KSEG1);
 
+#ifdef CONFIG_USE_OF
 	/*
 	 * Load the builtin devicetree. This causes the chosen node to be
 	 * parsed resulting in our memory appearing
@@ -174,18 +209,20 @@ void __init plat_mem_setup(void)
 	 */
 	__dt_setup_arch(__dtb_start);
 
+	/* TODO see phys 0x0318 to get mem bytes for CICs expect CIC-6105 */
+
 	of_scan_flat_dt(early_init_dt_find_memory, NULL/*unused by callback*/);
 	if (memory_dtb)
 		of_scan_flat_dt(early_init_dt_scan_memory, NULL/*unused by callback...by spec?*/);
-//	else if (soc_info.mem_size)
-//		add_memory_region(soc_info.mem_base, soc_info.mem_size * SZ_1M,
-//				BOOT_MEM_RAM);
 	else
-		detect_memory_region(/*mem_base*/0,
-				/*size_min*/4 * SZ_1M,
-				/*size_max*/8 * SZ_1M);
+#endif
+	{
+		/* n64load passes memory size by $a0, stored as fw_arg0 */
+		add_memory_region(/*base*/0, /*size*/fw_arg0, BOOT_MEM_RAM);
+	}
 }
 
+// called from arch/mips/kernel/time.c:time_init
 void __init plat_time_init(void)
 {
 //	struct clk *clk;
@@ -206,6 +243,7 @@ void __init plat_time_init(void)
 	clocksource_probe();
 }
 
+// called from arch/mips/mm/init.c:free_initmem
 void __init prom_free_prom_memory(void)
 {
 }
